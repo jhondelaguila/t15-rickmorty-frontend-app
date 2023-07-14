9839ca9d634fcc165deb324ea1956de2d0e761a1
Jhon Junior del Águila Pinedo <jhonjr.dap@gmail.com>
2023-07-14 19:16:49 +0200

    cambios character,OnInit

--- src/app/main/base/characters/character/character.component.ts
+++ src/app/main/base/characters/character/character.component.ts
@@ -1,17 +1,17 @@
-import { Component,Input } from '@angular/core';
+import { Component,Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-character',
   templateUrl: './character.component.html',
   styleUrls: ['./character.component.css']
 })
-export class CharacterComponent {
+export class CharacterComponent implements OnInit {
   @Input() listaCharacters:any[] = [];
   firstRow:any = [];
   secondRow:any = [];
   i:number = 1;
 
-  ngOnInit(){
+  ngOnInit():void{
     for (let index = 0; index < 4; index++) {
       this.firstRow.push(this.listaCharacters[index]);
       this.secondRow.push(this.listaCharacters[index+4]);
